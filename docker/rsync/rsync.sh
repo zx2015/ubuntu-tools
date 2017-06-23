713decb53cc71dfd2c4f24dd71589bda6866a863
root <root@nsn-china-hangzhou-binjiang-vlan30023153.china.nsn-net.net>
2017-06-23 17:19:57 +0800

    1. change entry script to copy config file to etc; 2. default configuration

--- docker/rsync/rsync.sh
+++ docker/rsync/rsync.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-exec ionice -c 3 /usr/bin/rsync --daemon </dev/null
+#Copy configure files to right place
+cp -f /config/rsyncd.conf /etc/rsyncd.conf
+cp -f /config/rsyncd.secrets /etc/rsyncd.secrets
+chmod 600 /etc/rsyncd.secrets
+
+#start rsync service
+exec /usr/bin/rsync --no-detach --daemon --config /etc/rsyncd.conf "$@"
